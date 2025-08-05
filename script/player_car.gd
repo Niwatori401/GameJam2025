01b48eafcb62b4442c750990e960e41a5b0f57e8
Niwatori401 <->
2025-08-04 18:19:19 -0700

    Add directional shooting

--- script/player_car.gd
+++ script/player_car.gd
@@ -35,13 +35,13 @@ func _process(delta: float) -> void:
 	if is_dead:
 		return;
 	
-	var movement_direction = get_direction_from_inputs()
+	var movement_direction = get_move_direction_from_inputs()
 	var velocity_target = Vector2(delta * speed * movement_direction.x, delta * speed * movement_direction.y * vertical_damping);
 	move_and_collide(velocity_target);
 	
-	fire_bullets_if_firing();
+	fire_bullets_if_firing(get_aim_direction_from_inputs());
 
-func get_direction_from_inputs() -> Vector2:
+func get_move_direction_from_inputs() -> Vector2:
 	var movement = Vector2();
 	if Input.is_action_pressed("up"):
 		movement.y += -1;
@@ -54,18 +54,42 @@ func get_direction_from_inputs() -> Vector2:
 	
 	return movement;
 	
+
+func get_aim_direction_from_inputs() -> Vector2:
+	var aim = Vector2();
+	if Input.is_action_pressed("aim_up"):
+		aim.y += -1;
+	if Input.is_action_pressed("aim_down"):
+		aim.y += 1;
+	if Input.is_action_pressed("aim_right"):
+		aim.x += 1;
+	if Input.is_action_pressed("aim_left"):
+		aim.x += -1;
 	
-func fire_bullets_if_firing():
+	return aim;
+
+func fire_bullets_if_firing(aim_direction : Vector2):
 	if !Input.is_action_just_pressed("fire_weapon"):
 		return;
-		
+	
+	var aim_unit_vector;
+	if aim_direction.x != 0:
+		const DIAGONAL_ANGLE_OFFSET_MAGNITUDE = deg_to_rad(30);
+		aim_unit_vector = Vector2(cos(DIAGONAL_ANGLE_OFFSET_MAGNITUDE) * aim_direction.x, sin(DIAGONAL_ANGLE_OFFSET_MAGNITUDE) * aim_direction.y);
+	elif aim_direction.x == 0 and aim_direction.y == 0:
+		aim_unit_vector = Vector2(-1, 0);
+	else:
+		aim_unit_vector = aim_direction;
+
+	aim_unit_vector = aim_unit_vector.normalized();
+	
 	var bullet_instance = BULLET.instantiate();
 	# This could cause issues if there is not parent, but in practice that shouldn't happen.
 	# Adding as child of car causes bullets to "vibrate"
 	get_parent().add_child(bullet_instance);
 	const DEPSAWN_SECONDS = 3.0;
 	get_tree().create_timer(DEPSAWN_SECONDS).timeout.connect(func(): if bullet_instance != null: bullet_instance.queue_free())
-	Utility.teleport(bullet_instance, $BulletSpawnPoint.global_position, Vector2(-BULLET_SPEED, 0));
+	Utility.teleport(bullet_instance, $BulletSpawnPoint.global_position, BULLET_SPEED * aim_unit_vector);
 	
 func get_global_coords_for_car_center() -> Vector2:
 	var center_offset = Vector2($CarHitbox.shape.get_rect().size) * Vector2(0.5, 0.5);
@@ -87,9 +111,6 @@ func update_sprite():
 			
 	
 	$CarSprite.texture = graphic_list[len(graphic_list) - 1];
-	
-	
-	
 	
 func die():
 	is_dead = true;
